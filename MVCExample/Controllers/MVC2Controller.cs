8f25e43a0bb6fa309392293cdf188afc09f1016a
chegeni-0608 <mmoa80610@gmail.com>
2024-11-19 15:44:25 +0330

    Add project files.

---
+++ MVCExample/Controllers/MVC2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample.Controllers
+{
+    public class MVC2Controller : Controller
+    {
+        // GET: MVC2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
